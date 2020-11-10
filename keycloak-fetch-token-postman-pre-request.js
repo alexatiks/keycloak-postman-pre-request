4d8c1360b25b7095e7563e3201a0095606236d9e
SirSundays <49527200+SirSundays@users.noreply.github.com>
2020-11-10 14:53:56 +0100

    Update to password authentication (#1)
    * Update the Pre-Request-Script and a Link in README.md to latest Keycloak
    
    The Pre-Request-Script got updated so it now uses a real user. This has the advantage, that you can use roles and such.
    The link to the documentation of Keycloak was pointing to an non existing page. Updated that to a link in the latest documentation.
    
    * Updated the Screenshots
    
    * Update README.md
    
    Now with more detail.

--- keycloak-fetch-token-postman-pre-request.js
+++ keycloak-fetch-token-postman-pre-request.js
@@ -1,12 +1,18 @@
-var server       = "";
-var realm        = "";
-var grantType    = "client_credentials";
-var clientId     = "";
-var clientSecret = "";
+var server       = ""; // add your Keycloak-URL here (without /auth)
+var realm        = ""; // the name of the realm
+var grantType    = "password"; // the granttype, with password you can login as a normal user
+var clientId     = ""; // the name of the client you created in Keycloak
+var clientSecret = ""; // the secret you copied earlier
+var username     = ""; // the username of the user you want to test with
+var password     = ""; // the password of the user you want to test with
 
+// creating the request URL
 var url  = `${server}/auth/realms/${realm}/protocol/openid-connect/token`;
-var data = `grant_type=${grantType}&client_id=${clientId}&client_secret=${clientSecret}`;
+// creating the body of the request
+var data = `grant_type=${grantType}&client_id=${clientId}&username=${username}&password=${password}&client_secret=${clientSecret}`;
 
+// request to Keycloak
+// read more about this here: https://www.keycloak.org/docs/latest/authorization_services/#_service_overview
 pm.sendRequest({
     url: url,
     method: 'POST',
@@ -16,10 +22,10 @@ pm.sendRequest({
         raw: data
     }
 },  function(err, response) {
-    
+    // Set the environment variable for token
     var response_json = response.json();
     var token = response_json.access_token;
     pm.environment.set('token', token);
+    // You can open up the console in Postman with Alt + Ctrl + C
     console.log(token);
 });
-
